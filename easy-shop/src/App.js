432d160f17394dcfb9fd80a430507f3d634ea61c
Arnold-In-Tech <g16a7782@gmail.com>
2024-02-14 23:03:33 +0300

    updated routes

--- easy-shop/src/App.js
+++ easy-shop/src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import LoginUser from './components/Login';
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<LoginUser />} />
       </Routes>
     </>
   );
