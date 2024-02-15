76b69a57fe4e24bdafdf0688cc6859ab57b66edf
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-02-15 12:20:28 +0300

    Updates

--- easy-shop/src/components/SignIn.js
+++ easy-shop/src/components/SignIn.js
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
-import '../styling/login.css';
-import { useNavigate } from 'react-router-dom';
-
-// =============================================//
-// Login page
-// prepared by Arnold
-// Date: 14/02/2024
-// =============================================//
+import React, { useState } from "react";
+import "../styling/login.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginUser() {
-
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: "",
@@ -17,11 +10,10 @@ function LoginUser() {
   });
   const [checked, setChecked] = useState(true);
 
-  function handleCheckChange(event){
+  function handleCheckChange(event) {
     event.target.checked = false;
     setChecked(!checked);
-  }     
-
+  }
 
   function handleChange(e) {
     setFormData({
@@ -32,72 +24,78 @@ function LoginUser() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    navigate('/Home')
-
-    // We will not be persisting the login data
-    // fetch("http://localhost:3000/LoginTokens", {   // check login db.json
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(formData),
-    // })
-    //   .then((r) => r.json())
-    //   .then((user) => {
-    //     // onLogin(user);
-    //     // after logging the user in, redirect to the home page!
-    //     navigate('/ProductsList');
-    // });
-    //===========================
+    navigate("/");
   }
 
   return (
     <div className="login-wrapper">
-      <form id="login-form" action="" className="animate" onSubmit={handleSubmit}>
+      <form
+        id="login-form"
+        action=""
+        className="animate"
+        onSubmit={handleSubmit}
+      >
         <div id="image-container">
-          <img id="login-avatar" alt="login-avatar" src={ require('../images/logo.png') } />
+          <img
+            id="login-avatar"
+            alt="login-avatar"
+            src={require("../images/logo.png")}
+          />
         </div>
         <div className="container">
-        <h1>Log In</h1>
-          <hr/>
-        <label>
-          <p><strong>Username</strong></p>
-          <input
-            placeholder="Enter Password"
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </label>
-
-        <label>
-            <p><strong>Password</strong></p>
-        <input
-          placeholder="Enter Password"
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        </label>
-        <div>
-          <button type="submit" onClick={handleSubmit}>Login</button>
-        </div>
-        <label>
-          <input type="checkbox" checked={checked} onChange={handleCheckChange} name="rememberme" />
-          Remember me
+          <h1>Log In</h1>
+          <hr />
+          <label>
+            <p>
+              <strong>Username</strong>
+            </p>
+            <input
+              placeholder="Enter Password"
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
           </label>
-        <span className="passwd">Forgot <a href="#">password?</a></span>  
-        <p>
-          <a href="#">Register here</a>
-        </p>
-      </div>
+
+          <label>
+            <p>
+              <strong>Password</strong>
+            </p>
+            <input
+              placeholder="Enter Password"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <div>
+            <button type="submit" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+          <label>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={handleCheckChange}
+              name="rememberme"
+            />
+            Remember me
+          </label>
+          <span className="passwd">
+            Forgot <a href="#">password?</a>
+          </span>
+          <p>
+            <a href="#">Register here</a>
+          </p>
+        </div>
       </form>
     </div>
   );
 }
 
-export default LoginUser
+export default LoginUser;
